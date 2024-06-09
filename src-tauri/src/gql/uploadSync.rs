5c9c183cfe3db80e2d9eafbdcfccec8d1bb81f8a
Alex Goodman <alexthegoodman@gmail.com>
2024-06-08 20:17:21 -0400

    switch to tokio in background and send new files to api

---
+++ src-tauri/src/gql/uploadSync.rs
@@ -0,0 +1,61 @@
+use reqwest_graphql::Client;
+use serde::{Deserialize, Serialize};
+use std::{collections::HashMap, fmt::format, str::FromStr};
+
+#[derive(Deserialize)]
+pub struct Data {
+    pub uploadSync: String,
+}
+
+// #[derive(Deserialize)]
+// pub struct User {
+//     id: String,
+//     name: String,
+// }
+
+#[derive(Serialize)]
+pub struct Vars {
+    // id: u32,
+    fileName: String,
+    filePath: String,
+    fileData: String,
+}
+
+pub async fn upload_sync_one_file(
+    auth_token: String,
+    file_name: String,
+    file_path: String,
+    file_data: String,
+) -> Result<Data, Box<dyn std::error::Error>> {
+    let endpoint = "http://localhost:4000/graphql";
+    let query = r#"
+        mutation UploadSync($fileName: String!, $filePath: String!, $fileData: String!) {
+            uploadSync(fileName: $fileName, filePath: $filePath, fileData: $fileData)
+        }
+   "#;
+
+    let auth_header = "Bearer ".to_owned() + &auth_token.clone();
+    let auth_header_str: &str = &auth_header[..];
+
+    let mut headers = HashMap::new();
+    headers.insert("Authorization", auth_header_str);
+
+    let client = Client::new_with_headers(endpoint, headers);
+
+    let vars = Vars {
+        fileName: file_name,
+        filePath: file_path,
+        fileData: file_data,
+    };
+
+    println!("Making call...");
+
+    let data = client
+        .query_with_vars::<Data, Vars>(query, vars)
+        .await
+        .unwrap();
+
+    // println!("Id: {}, Name: {}", data.user.id, data.user.name);
+
+    Ok(data)
+}
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,69 +6,144 @@
 //     format!("Hello, {}! You've been greeted from Rust!", name)
 // }
 
+mod gql;
+
+use notify::EventKind;
 use std::fs;
-use std::path::PathBuf;
 // use tauri::api::path::{app_data_dir, local_data_dir};
 use tauri::api::path::{app_data_dir, resolve_path, BaseDirectory};
 
+use std::sync::Arc;
 use std::sync::Mutex;
-use tauri::{AppHandle, Manager};
+use tauri::{App, AppHandle, Manager};
 
 use async_channel::{unbounded, Receiver};
-use async_std::task;
 use futures::stream::StreamExt;
 use notify::{Config, Event, RecommendedWatcher, RecursiveMode, Watcher};
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::pin::Pin;
+use tokio::time::{sleep, Duration};
 
-async fn watch_directory(mut events: Pin<Box<Receiver<notify::Result<Event>>>>) {
+use crate::gql::uploadSync::upload_sync_one_file;
+
+async fn watch_directory(
+    mut events: Pin<Box<Receiver<notify::Result<Event>>>>,
+    auth_token: String,
+    sync_dir: PathBuf,
+) {
     while let Some(event) = events.next().await {
         match event {
             Ok(event) => {
-                println!("Event: {:?}", event);
+                println!("Sync Folder Event: {:?}", event);
 
-                // Trigger your sync process here based on the event
-                sync_files().await;
+                if (event.kind == EventKind::Create(notify::event::CreateKind::Any)) {
+                    // Trigger your sync process here based on the event
+                    // sync_files(auth_token.clone(), event.paths, &sync_dir).await;
+
+                    // Filter out directories and only process files
+                    let file_paths: Vec<PathBuf> = event
+                        .paths
+                        .iter()
+                        .filter_map(|path| {
+                            if fs::metadata(path)
+                                .map(|metadata| metadata.is_file())
+                                .unwrap_or(false)
+                            {
+                                Some(path.clone())
+                            } else {
+                                None
+                            }
+                        })
+                        .collect();
+
+                    // Trigger your sync process here based on the filtered file paths
+                    if !file_paths.is_empty() {
+                        println!("Create and sync triggered...");
+
+                        sync_files(auth_token.clone(), file_paths, &sync_dir).await;
+                    }
+                }
             }
             Err(e) => println!("Watch error: {:?}", e),
         }
     }
 }
 
-async fn sync_files() {
-    // Your sync logic goes here
-    println!("Syncing files...");
+async fn sync_files(auth_token: String, paths: Vec<PathBuf>, sync_dir: &Path) {
+    for path in paths {
+        if let Ok(relative_path) = path.strip_prefix(sync_dir) {
+            // Extract the directory path and file name
+            if let Some(directory) = relative_path.parent() {
+                if let Some(file_name) = relative_path.file_name() {
+                    let directory_str = directory.to_string_lossy().into_owned();
+                    let file_name_str = file_name.to_string_lossy().into_owned();
+
+                    // Log the relative directory and file name
+                    // println!("Directory: {}, File name: {}", directory_str, file_name_str);
+
+                    // Read the file content
+                    let file_content = std::fs::read(&path)
+                        .map(base64::encode)
+                        .unwrap_or_else(|_| "".to_string());
+
+                    // Log the file content
+                    println!(
+                        "Syncing file: {:?} from {:?} with content: {}",
+                        file_name_str, directory_str, file_content
+                    );
+
+                    // logic to upload `file_content` to S3
+                    let _ = upload_sync_one_file(
+                        auth_token.clone(),
+                        file_name_str,
+                        directory_str,
+                        file_content,
+                    )
+                    .await;
+                } else {
+                    println!("Could not extract file name from path: {:?}", relative_path);
+                }
+            } else {
+                println!("Could not extract directory from path: {:?}", relative_path);
+            }
+        } else {
+            println!(
+                "Path {:?} is not within base directory {:?}",
+                path, sync_dir
+            );
+        }
+    }
 }
 
-// #[async_std::main]
-async fn watch_folder() -> notify::Result<()> {
+async fn watch_folder(auth_token: String, sync_dir: PathBuf) -> notify::Result<()> {
     let (tx, rx) = unbounded();
     let watcher_config = Config::default()
         .with_poll_interval(std::time::Duration::from_secs(2))
         .with_compare_contents(true);
     let mut watcher: RecommendedWatcher = Watcher::new(
         move |res| {
-            task::block_on(async {
-                tx.send(res).await.unwrap();
+            // Block on sending the result through the async channel
+            tokio::task::block_in_place(|| {
+                tx.send_blocking(res).unwrap();
             });
         },
         watcher_config,
     )?;
 
-    watcher.watch(
-        Path::new("C:/Users/alext/CommonOSFiles"),
-        RecursiveMode::Recursive,
-    )?;
+    watcher.watch(&sync_dir, RecursiveMode::Recursive)?;
 
     // Pin the receiver
     let pinned_rx = Box::pin(rx);
 
     // Spawn the directory watcher as an async task
-    task::spawn(watch_directory(pinned_rx));
+    tokio::task::spawn(async move {
+        watch_directory(pinned_rx, auth_token, sync_dir).await;
+    });
 
     // Keep the main function alive to listen for events
     loop {
-        task::sleep(std::time::Duration::from_secs(60)).await;
+        // task::sleep(std::time::Duration::from_secs(60)).await;
+        sleep(Duration::from_secs(60)).await;
     }
 }
 
@@ -99,7 +174,22 @@ fn save_auth_token(state: tauri::State<'_, AppState>, token: String) -> Result<(
     Ok(())
 }
 
-#[async_std::main]
+fn read_auth_token(config: &Arc<tauri::Config>) -> String {
+    println!("read_auth_token");
+
+    let app_data_path = app_data_dir(config)
+        .ok_or("Failed to get AppData directory (1)")
+        .expect("Failed to get AppData directory (2)");
+    let read_path = app_data_path.join("auth");
+
+    // pull String content from read_path
+    let auth_data =
+        String::from_utf8_lossy(&std::fs::read(read_path).unwrap_or_default()).to_string();
+
+    auth_data
+}
+
+#[tokio::main]
 async fn main() {
     // tauri::Builder::default()
     //     .invoke_handler(tauri::generate_handler![greet])
@@ -111,13 +201,18 @@ async fn main() {
     tauri::Builder::default()
         .setup(|app| {
             let handle = app.handle();
+            let config = handle.config();
+
             app.manage(AppState { handle });
 
+            let auth_token = read_auth_token(&config);
+            let sync_dir = PathBuf::from("C:/Users/alext/CommonOSFiles");
+
             // Spawn the folder watcher task
-            task::spawn(async {
+            tokio::task::spawn(async move {
                 println!("Starting Folder Watch...");
 
-                if let Err(e) = watch_folder().await {
+                if let Err(e) = watch_folder(auth_token, sync_dir).await {
                     println!("Error watching folder: {:?}", e);
                 }
             });
